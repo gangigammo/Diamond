e85fbe023677066264d9d3fa5ca2d2570b83b944
Durun <NegiIgaiNuki@gmail.com>
2018-12-07 01:33:20 +0900

    他のOSにも対応

--- Diamond/financial/tests.py
+++ Diamond/financial/tests.py
@@ -1,26 +1,52 @@
 from django.test import TestCase
 from selenium import webdriver
-from enum import Enum
+from selenium.common.exceptions import WebDriverException
+from enum import IntEnum, auto
 import os
 import pathlib
 
 # Create your tests here.``
 
 # ブラウザの列挙
-class Browser(Enum):
-		Chrome = 1
+class Browser(IntEnum):
+		Chrome = auto()
+
 # ブラウザのドライバーを新しく取得する
-class Drivers():
+class Drivers:
 	# ドライバーがあるパスを指定
-	dir = pathlib.Path('../Diamond/browsers')
+	dir_path = '../Diamond/browsers/'
 	# ドライバー本体のファイル名を指定
-	bin = {Browser.Chrome: 'chromedriver'}
+	bin_paths = {Browser.Chrome: 'chromedriver'}
+
+	# ドライバーの取得関数
+	factories = {Browser.Chrome: webdriver.Chrome}
+	# OSによって異なる実行ファイルの拡張子
+	suffix = {'nt': '.exe', 'posix': ''}
+
+	# テスト用ブラウザを取得する
+	@classmethod
+	def get(cls, browser: Browser):
+		path = cls.dir_path + cls.bin_paths[browser] + cls.suffix[os.name]
+		abs_path = pathlib.Path(path).resolve()
+		try:
+			driver = cls.factories[browser](str(abs_path))
+		except FileNotFoundError as e:
+			print(e)
+			message = 'ブラウザのドライバ ' + abs_path + ' が存在しません.'
+			raise FileNotFoundError(message)
+		except WebDriverException as e:
+			print(e)
+			message = 'ブラウザ ' + browser.name + ' はインストールされていません.'
+			raise EnvironmentError(message)
+		return driver
 
 
 class MyTests(TestCase):
 	def test_sample(self):
-		driverPath = pathlib.Path('../Diamond/tests/chromedriver.exe').resolve()
-		print('driver:	', driverPath)
-		print('exists?:	', os.path.exists(driverPath))
-		browser = webdriver.Chrome(str(driverPath))
-		browser.get('http://seleniumhq.org/')
+		try:
+			driver = Drivers.get(Browser.Chrome)
+		except Exception as e:
+			print(e)
+			message = 'テスト用ブラウザを初期化できませんでした.'
+			raise SystemExit(message)
+		driver.get('http://seleniumhq.org/')
