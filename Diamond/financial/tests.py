929ac7de42517a75aeac60d68b1da922a359a8e6
Durun <NegiIgaiNuki@gmail.com>
2018-12-07 02:06:40 +0900

    Edgeにも対応

--- Diamond/financial/tests.py
+++ Diamond/financial/tests.py
@@ -10,19 +10,27 @@ import pathlib
 # ブラウザの列挙
 class Browser(IntEnum):
 		Chrome = auto()
+		Edge = auto()
+		Firefox = auto()
 
 # ブラウザのドライバーを新しく取得する
 class Drivers:
 	# ドライバーがあるパスを指定
 	dir_path = '../Diamond/browsers/'
 	# ドライバー本体のファイル名を指定
-	bin_paths = {Browser.Chrome: 'chromedriver'}
+	bin_paths = {Browser.Chrome: 'chromedriver',
+              Browser.Edge: 'MicrosoftWebDriver',
+              Browser.Firefox: 'geckodriver'}
 
 	# ダウンロードURLのメモ
 	bin_urls = {
-            Browser.Chrome: 'https://sites.google.com/a/chromium.org/chromedriver/downloads'}
+            Browser.Chrome: 'https://sites.google.com/a/chromium.org/chromedriver/downloads',
+         			Browser.Edge: 'https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/',
+      				Browser.Firefox: 'https://github.com/mozilla/geckodriver/releases'}
 	# ドライバーの取得関数
-	factories = {Browser.Chrome: webdriver.Chrome}
+	factories = {Browser.Chrome: webdriver.Chrome,
+		Browser.Edge: webdriver.Edge,
+		Browser.Firefox: webdriver.Firefox}
 	# OSによって異なる実行ファイルの拡張子
 	suffix = {'nt': '.exe', 'posix': ''}
 
