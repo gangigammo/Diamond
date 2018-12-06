331b24fe54e44cd2ad2d36973c66cc6845b7383d
Durun <NegiIgaiNuki@gmail.com>
2018-12-06 22:57:22 +0900

    Chromeが勝手に起動して消えるだけ(Windows)

--- Diamond/financial/tests.py
+++ Diamond/financial/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from selenium import webdriver
+import os
+import pathlib
 
-# Create your tests here.
+# Create your tests here.``
+
+
+class MyTests(TestCase):
+	def test_sample(self):
+		driverPath = pathlib.Path('../Diamond/tests/chromedriver.exe').resolve()
+		print('driver:	', driverPath)
+		print('exists?:	', os.path.exists(driverPath))
+		browser = webdriver.Chrome(str(driverPath))
+		browser.get('http://seleniumhq.org/')
