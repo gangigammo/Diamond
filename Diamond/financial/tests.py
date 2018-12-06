29d5197bd72377844ab101bba24b1dbd41410aa5
Durun <NegiIgaiNuki@gmail.com>
2018-12-06 23:52:17 +0900

    webドライバーのファクトリークラス

--- Diamond/financial/tests.py
+++ Diamond/financial/tests.py
@@ -1,9 +1,20 @@
 from django.test import TestCase
 from selenium import webdriver
+from enum import Enum
 import os
 import pathlib
 
 # Create your tests here.``
+
+# ブラウザの列挙
+class Browser(Enum):
+		Chrome = 1
+# ブラウザのドライバーを新しく取得する
+class Drivers():
+	# ドライバーがあるパスを指定
+	dir = pathlib.Path('../Diamond/browsers')
+	# ドライバー本体のファイル名を指定
+	bin = {Browser.Chrome: 'chromedriver'}
 
 
 class MyTests(TestCase):
