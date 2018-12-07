29d6a77887e61478fe984c39d975fbde34dee2dc
yamagata-eisuke <yamagata.e.ab@m.titech.ac.jp>
2018-12-07 11:05:11 +0900

    モデルにユーザー足しました

--- Diamond/financial/models.py
+++ Diamond/financial/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+
 class Income(models.Model): #収入のモデル
     description = models.CharField(max_length=128)
     amount = models.IntegerField()
 
+
 class Expense(models.Model): #支出のモデル
     description = models.CharField(max_length=128)
     amount = models.IntegerField()
+
+
+class User(models.Model): #ユーザーのモデル
+    name = description = models.CharField(max_length=128)
+    password = description = models.CharField(max_length=128)
