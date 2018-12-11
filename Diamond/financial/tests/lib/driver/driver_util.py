acc48b10a4b6ba2595bf809720929147dfc1b7cd
Durun <NegiIgaiNuki@gmail.com>
2018-12-12 00:46:23 +0900

    フォーム入力

--- Diamond/financial/tests/lib/driver/driver_util.py
+++ Diamond/financial/tests/lib/driver/driver_util.py
@@ -15,9 +15,28 @@ def click_url(self, partial_url: str):
     elements[0].click()
 
 
+def form_input(self, form_id: str, input_dict: dict):
+    forms = self.find_elements_by_xpath("//form[@id='" + form_id + "']")
+    if (len(forms) == 0):
+        raise NoSuchElementException('フォーム ' + form_id + ' が見つかりません.')
+    elif (len(forms) > 1):
+        raise SyntaxWarning('フォーム ' + form_id + ' が複数あります.')
+    form = forms[0]
+    for name, value in input_dict.items():
+        elements = form.find_elements_by_name(name)
+        if (len(elements) == 0):
+            raise NoSuchElementException('入力欄 ' + name + ' が見つかりません.')
+        elif (len(elements) > 1):
+            raise SyntaxWarning('入力欄 ' + name + ' が複数あります.')
+        elements[0].send_keys(value)
+    form.submit()
+
 # メソッドをWebDriverに追加
+
+
 def __setattr(method):
     setattr(WebDriver, method.__name__, method)
 
 
 __setattr(click_url)
+__setattr(form_input)
--- Diamond/financial/tests/lib/signup.py
+++ Diamond/financial/tests/lib/signup.py
@@ -5,3 +5,5 @@ from .driver import driver_util
 
 def signup(driver):
     driver.click_url("signup")
+    input_dict = {"name": "Taro", "password": "yahoo"}
+    driver.form_input("signupForm", input_dict)
