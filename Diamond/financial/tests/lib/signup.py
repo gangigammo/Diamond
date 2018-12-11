8bd29d03a61078f8bee208bc590b01fd3f86b3e8
Durun <NegiIgaiNuki@gmail.com>
2018-12-12 04:39:58 +0900

    ログイン関連の操作

--- Diamond/financial/tests/lib/signup.py
+++
@@ -1,9 +0,0 @@
-from .driver import driver_util
-
-# サインアップする
-
-
-def signup(driver):
-    driver.click_url("signup")
-    input_dict = {"name": "Taro", "password": "yahoo"}
-    driver.form_input("signupForm", input_dict)
