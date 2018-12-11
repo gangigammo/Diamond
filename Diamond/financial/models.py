cf476b49b3225aa1826f822b998518289f95d7fc
Katsumata3505 <katsumata.y.ab@m.titech.ac.jp>
2018-12-11 11:25:31 +0900

    収支モデルをBalanceに統一

--- Diamond/financial/models.py
+++ Diamond/financial/models.py
@@ -3,22 +3,22 @@ from django.db import models
 # Create your models here.
 
 
-class Income(models.Model): #収入のモデル
+class Balance(models.Model):  # 収支のモデル
     description = models.CharField(max_length=128)
-    amount = models.IntegerField()
+    amount = models.PositiveIntegerField()
+    date = models.DateField()
+    isIncome = models.BooleanField()
     categoryName = models.CharField(max_length=128)
 
-
-class Expense(models.Model): #支出のモデル
-    description = models.CharField(max_length=128)
-    amount = models.IntegerField()
 
 class User(models.Model): #ユーザーのモデル
     name = models.CharField(max_length=128)
     password = models.CharField(max_length=128)
 
+
 class IncomeCategory(models.Model):
     categoryName = models.CharField(max_length=128)
+
 
 class ExpenseCategory(models.Model):
     categoryName = models.CharField(max_length=128)
