61aebddc557916fc39fcb7f89c13befb94ffea0e
Durun <NegiIgaiNuki@gmail.com>
2018-12-11 11:31:58 +0900

    note

--- Diamond/financial/tests/lib/ui.py
+++ Diamond/financial/tests/lib/ui.py
@@ -18,12 +18,8 @@ def test_sample():
         message = 'テスト用ブラウザを初期化できませんでした.'
         print(message)
         raise SystemExit(message)
-    driver.implicitly_wait(10)  # seconds
+    driver.implicitly_wait(10)  # ブラウザ処理の待機時間(seconds)
     driver.get('http://127.0.0.1:8000')
     driver.find_element_by_xpath("/html/body/a").click()
     driver.close()
     driver.quit()
-
-
-def sample():
-    print('unko')
