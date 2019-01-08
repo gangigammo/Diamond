f1b6934c11c5a2d8110764dd3e68a8f66a711b9c
Durun <negiigainuki@gmail.com>
2019-01-09 02:52:28 +0900

    nullチェック

--- Diamond/financial/templatetags/toTableFilter.py
+++ Diamond/financial/templatetags/toTableFilter.py
@@ -5,6 +5,9 @@ from typing import Optional
 
 @register.filter
 def balanceToRow(value: Balance) -> str:
-    if (type(value) is not Balance):
-        raise TypeError
-    return str(value.amount) + str(value.description) + str(value.categoryName)
+    if not(type(value) in (Balance, type(None))):
+        raise TypeError("toRowフィルタに渡す値の型が違います")
+    string = value and str(value.amount) + \
+        str(value.description) + str(value.categoryName)
+    string = string or "none"
+    return string
