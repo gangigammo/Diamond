23eee5992dbcc96b33bfd8929e31ff07b2a17c9d
Durun <negiigainuki@gmail.com>
2019-01-09 00:55:40 +0900

    削除対象が存在しないときにメッセージ出力

--- Diamond/financial/view/balance/delete.py
+++ Diamond/financial/view/balance/delete.py
@@ -3,7 +3,11 @@ from financial.models import Balance
 from financial.view.util.balances import Balances
 
 
+template = "balance/delete.html"
+
+
 def start(request, balance_id):
     # TODO テスト用ページを返している
     balance = Balances.getById(balance_id)
-    return render(request, "balance/delete.html", {"balance": balance})
+    message = not balance and "対象が存在しません"
+    return render(request, template, {"balance": balance, "message": message})
