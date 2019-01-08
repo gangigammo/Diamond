ec0bd97e8d6e69206cba6e25993daae088f28f61
Durun <negiigainuki@gmail.com>
2019-01-09 03:25:34 +0900

    toRowフィルタ 型で判別

--- Diamond/financial/templatetags/toTableFilter.py
+++ Diamond/financial/templatetags/toTableFilter.py
@@ -1,13 +1,37 @@
 from django.template.defaultfilters import register
 from financial.models import Balance
+from typing import Union
 from typing import Optional
+
+NoneType = type(None)
+
+
+# テーブルの行を生成
+
+
+def __noneToRow(value: NoneType) -> str:
+    return "none"
+
+
+def __balanceToRow(value: Balance) -> str:
+    string = value and str(value.amount) + \
+        str(value.description) + str(value.categoryName)
+    return string
+
+
+# ディスパッチャ
+
+
+__toRowMethods = {
+    NoneType:   __noneToRow,
+    Balance:    __balanceToRow
+}
 
 
 @register.filter
-def balanceToRow(value: Balance) -> str:
-    if not(type(value) in (Balance, type(None))):
-        raise TypeError("toRowフィルタに渡す値の型が違います")
-    string = value and str(value.amount) + \
-        str(value.description) + str(value.categoryName)
-    string = string or "none"
+def toRow(value: Optional[Union[Balance]]) -> str:
+    method = __toRowMethods.get(type(value))
+    if (method is None):
+        raise TypeError("toRowフィルタに渡す値の型が違います" + type(value))
+    string = method(value)
     return string
