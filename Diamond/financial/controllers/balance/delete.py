a524562fa746aea014166cce9974e620d6131faa
Durun <negiigainuki@gmail.com>
2019-01-08 14:47:32 +0900

    URLで選択した収支の内容を表示

--- Diamond/financial/controllers/balance/delete.py
+++ Diamond/financial/controllers/balance/delete.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from financial.models import *
+from financial.models import Balance
 
 
 def start(request, balance_id):
     # TODO テスト用ページを返している
-    return render(request, "balance/delete.html", {"id": balance_id})
+    balances = Balance.objects.filter(id=balance_id)
+    for balance in balances:    # TODO コレクションからの取り出し
+        return render(request, "balance/delete.html", {"balance": balance})
