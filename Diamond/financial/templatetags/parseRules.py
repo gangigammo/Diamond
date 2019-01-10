2234208c57464a29121452f0f56d6bba17fab432
Durun <negiigainuki@gmail.com>
2019-01-10 15:45:54 +0900

    HTMLを扱う補助メソッド を util/html.py に分けた

--- Diamond/financial/templatetags/parseRules.py
+++ Diamond/financial/templatetags/parseRules.py
@@ -1,7 +1,6 @@
-from django.utils.safestring import SafeText, mark_safe
-from typing import Union, Dict, Optional, Any
-from django.utils.html import format_html, format_html_join, conditional_escape
+from typing import Union
 from financial.models import Balance
+from .util import *
 
 # 型の定義
 NoneType = type(None)               # nullを表す
@@ -31,27 +30,3 @@ defaultFormats = {
     NoneType: "",
     Balance: "amount,description,category,delete"
 }
-
-
-# 補助メソッド
-# aタグを生成
-def html_a(
-    content: str,    # 内容
-    href: str,      # リンク
-) -> SafeText:
-    attr = {"href": href}
-    return __generateHtml("a", content, attr)
-
-
-# HTMLタグを生成
-def __generateHtml(
-        tag: str,                               # HTMLタグ名
-        content: str,                           # 開始タグと終了タグの間に入る内容
-        attrs: Optional[Dict[str, Any]]          # 属性
-) -> SafeText:
-    htmlFormat = "<tag {}>{}</tag>".replace("tag", tag)
-    attrHtml = attrs and format_html_join(
-        " ", '{}="{}"', attrs.items())
-    attrHtml = attrHtml or ""
-    result = format_html(htmlFormat, attrHtml, content)
-    return result
