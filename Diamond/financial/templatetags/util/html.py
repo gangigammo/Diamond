f5c4728bd062917cfcfe26bec2b7436eb1795cc4
Durun <negiigainuki@gmail.com>
2019-01-10 16:37:04 +0900

    引数attrの渡し方を変更

--- Diamond/financial/templatetags/util/html.py
+++ Diamond/financial/templatetags/util/html.py
@@ -9,21 +9,23 @@ from django.utils.html import format_html, format_html_join
 # aタグを生成
 def html_a(
     content: str,    # 内容
-    href: str,      # リンク
+    href: str,       # リンク
+    **kwargs
 ) -> SafeText:
     attr = {"href": href}
-    return __generateHtml("a", content, attr)
+    attr.update(kwargs)
+    return __generateHtml("a", content, **attr)
 
 
 # HTMLタグを生成
 def __generateHtml(
-        tag: str,                               # HTMLタグ名
-        content: str,                           # 開始タグと終了タグの間に入る内容
-        attrs: Optional[Dict[str, Any]]          # 属性
+        tag: str,       # HTMLタグ名
+        content: str,   # 開始タグと終了タグの間に入る内容
+        **kwargs: str   # 属性
 ) -> SafeText:
     htmlFormat = "<tag {}>{}</tag>".replace("tag", tag)
-    attrHtml = attrs and format_html_join(
-        " ", '{}="{}"', attrs.items())
+    attrHtml = format_html_join(
+        " ", '{}="{}"', kwargs.items())
     attrHtml = attrHtml or ""
     result = format_html(htmlFormat, attrHtml, content)
     return result
