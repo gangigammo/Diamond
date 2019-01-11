9e9034e455763009828ad01b0772876b8823c015
Durun <negiigainuki@gmail.com>
2019-01-11 13:51:26 +0900

    チェックボックスによる削除機能

--- Diamond/financial/view/balanceEdit.py
+++ Diamond/financial/view/balanceEdit.py
@@ -1,5 +1,34 @@
 import financial.views
+from .util import *
+
+__topView = financial.views.view
 
 
+# ディスパッチャ
 def main(request):
-    return financial.views.view(request)
+    selects = __getSelects(request)
+    keys = request.POST.keys()
+
+    # keys の文字列によって操作を場合分け
+    if "delete" in keys:
+        result = __delete(request, selects)
+    # TODO elifで他のメソッドを追加
+
+    result = result or __topView(request)
+    return result
+
+
+# チェックボックスで選択した収支リストを取得
+def __getSelects(request):
+    query = request.POST
+    ids = query.getlist("balanceSelect")
+    selects = Balances.getByIdList(ids)
+    # TODO 他人の収支は除外
+    return selects
+
+
+# 収支を削除
+def __delete(request, selects):
+    for g in selects:
+        g.delete()
+    return __topView(request)
--- Diamond/financial/view/util/balances.py
+++ Diamond/financial/view/util/balances.py
@@ -1,8 +1,14 @@
 from financial.models import Balance
-from typing import Optional
+from typing import Optional, List
 
 
 class Balances():
+    # 収支idから収支を取得
     @staticmethod
     def getById(id: int) -> Optional[Balance]:      # 存在しなければNoneを返す
         return Balance.objects.filter(id=id).first()
+
+    # 収支idのリストから収支リストを取得
+    @staticmethod
+    def getByIdList(ids: List[int]) -> Optional[List[Balance]]:
+        return Balance.objects.filter(id__in=ids)
