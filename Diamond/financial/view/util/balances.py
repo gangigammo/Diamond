111907bc60417a09d048f08d1c7d2737ec7958c4
Durun <negiigainuki@gmail.com>
2019-01-11 14:34:22 +0900

    note 追記すべき場所にTODOと書いた

--- Diamond/financial/view/util/balances.py
+++ Diamond/financial/view/util/balances.py
@@ -2,12 +2,15 @@ from financial.models import Balance
 from typing import Optional, List
 
 
+# 条件に一致する収支を取得するstaticメソッド群
 class Balances():
     # 収支idから収支を取得
     @staticmethod
     def get(
         id=None,        # int
         writer=None     # str
+        # TODO 検索項目を追加できる
+
     ) -> Optional[Balance]:
         ids = id and [id]
         return Balances.getlist(ids, writer).first()
@@ -17,8 +20,13 @@ class Balances():
     def getlist(
         ids=None,       # List[int]
         writer=None     # str
+        # TODO 検索項目
+
     ) -> List[Balance]:
         all = Balance.objects
         byId = ids and all.filter(id__in=ids)
-        result = writer and byId and byId.filter(writer=writer)
+        byWriter = writer and byId and byId.filter(writer=writer)
+        # TODO 追加の検索
+
+        result = byWriter
         return result
