96714e348ae011070d596ed51bbf97aeadc1b0d2
Durun <negiigainuki@gmail.com>
2019-01-11 12:59:39 +0900

    wip

--- Diamond/financial/view/balanceEdit.py
+++ Diamond/financial/view/balanceEdit.py
@@ -1,5 +1,35 @@
 import financial.views
+from .util import *
+
+__topView = financial.views.view
 
 
 def main(request):
-    return financial.views.view(request)
+    # TODO ディスパッチ方法の洗練化
+    methodDict = {
+        "delete": __delete
+    }
+    methods = [*request.POST]
+    methods.remove("csrfmiddlewaretoken")
+    print(methods)
+    if len(methods) == 1:
+        method = methodDict.get(methods.pop())
+    else:
+        method = None
+    method = method or __pass
+    return method(request)
+
+
+def __pass(request):
+    return __topView(request)
+
+
+def __delete(request):
+    # TODO garbageNumsが取得できない
+    garbageNums = request.POST["balance_select"]
+    garbages = Balances.getByIdList(garbageNums)
+    print(garbageNums)
+    print(list(garbages))
+    for g in garbages:
+        g.delete()
+    return __topView(request)
