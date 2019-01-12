e8239201696a0e4cd4297ff9f41704eb1cb9748a
Durun <negiigainuki@gmail.com>
2019-01-12 17:48:43 +0900

    private関係フィールド (収支->カテゴリ)

--- Diamond/financial/models/balance.py
+++ Diamond/financial/models/balance.py
@@ -3,6 +3,8 @@
 """
 from django.db.models import Model
 from django.db.models import CharField, PositiveIntegerField, DateField, BooleanField
+from django.db.models import ForeignKey
+from django.db.models import CASCADE, SET_NULL
 
 
 class Balance(Model):
@@ -27,3 +29,7 @@ class Balance(Model):
     isIncome = BooleanField()
     categoryName = CharField(max_length=128)
     writer = CharField(max_length=128)
+
+    # private fields
+    # もとのカテゴリ削除時 -> __category=nullとなる
+    __category = ForeignKey("Category", on_delete=SET_NULL)
