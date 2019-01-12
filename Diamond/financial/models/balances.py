23a32ad68d8e78db1d0f0a38681927b9afdc66ee
Durun <negiigainuki@gmail.com>
2019-01-12 16:36:09 +0900

    説明書きをdocstringの形式に修正

--- Diamond/financial/models/balances.py
+++ Diamond/financial/models/balances.py
@@ -9,26 +9,32 @@ from typing import Optional, List
 class Balances():
     """
         収支を取得するためのstaticメソッド群です。
-        メソッド一覧
-        - get 条件に一致する収支を1つ取得
-        - getlist 条件に一致する収支のリストを取得
     """
 
     @staticmethod
     def get(
         # 検索項目
         # hoge=None
-        id=None,        # int
-        writer=None     # str
+        id=None,
+        writer=None
 
     ) -> Optional[Balance]:
         """
             データベース中から、条件に一致するBalanceを1つだけ返します。
             1つもなければNoneを返します。
             引数は全て、省略可です。全て省略された場合は全ての中から1つを返します。
-            引数:
-                - id: int 収支ID
-                - writer: str 収支のwriter
+
+            Parameters
+            ----------
+            id : int or None
+                収支ID
+            writer: str or None
+                収支のwriter
+
+            Returns
+            -------
+            result : Balance or None
+                条件に一致する収支。1つも一致しなければNone
         """
 
         ids = id and [id]
@@ -38,17 +44,26 @@ class Balances():
     def getlist(
         # 検索項目
         # hoge=None
-        ids=None,       # List[int]
-        writer=None     # str
+        ids=None,
+        writer=None
 
     ) -> List[Balance]:
         """
             データベース中から、条件に一致するBalanceのリスト返します。
             1つもなければ空リストを返します。
             引数は全て、省略可です。全て省略された場合は全てのBalanceを返します。
-            引数:
-                - ids: List[int] 収支IDのリスト
-                - writer: str 収支のwriter
+
+            Parameters
+            ----------
+            ids : list of int or None
+                収支IDのリスト
+            writer : str or None
+                収支のwriter
+
+            Returns
+            -------
+            result_list : List[Balance]
+                条件に一致する収支のリスト
         """
 
         all = Balance.objects
