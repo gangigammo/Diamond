0893d51c35cc8fb721b3e88527476ca6a2c43ecd
Durun <negiigainuki@gmail.com>
2019-01-12 20:38:33 +0900

    Userのイニシャライザ
    docstring

--- Diamond/financial/models/user.py
+++ Diamond/financial/models/user.py
@@ -18,17 +18,37 @@ class User(Model):
         パスワード
     """
 
-    # private field
+    # private fields
 
     _name = CharField(max_length=128)
     _password = CharField(max_length=128)  # TODO 平文にしない　
 
     # TODO acceser
 
-    # TODO initializer
+    # public methods
+
+    def __init__(self, name: str, password: str):
+        """
+        Userインスタンスを生成します
+
+        Parameters
+        ----------
+        name : str
+            ユーザー名
+        password : str
+            パスワード
+        """
+        _name = name
+        _password = self.__digest(password)
 
     # TODO __str__
 
     # TODO password auth
 
     # TODO update password
+
+    # private methods
+
+    def __digest(self, newPassword: str) -> str:
+        digested = newPassword  # TODO パスワードのダイジェスト化
+        return digested
