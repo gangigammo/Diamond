77e88334cb84770047cfe96e2d4da32bd0beb6e2
yoshikawajunpei <yoshikawa.j.ac@m.titech.ac.jp>
2019-01-24 22:33:23 +0900

    検索機能

--- Diamond/financial/migrations/0001_initial.py
+++ Diamond/financial/migrations/0001_initial.py
@@ -14,6 +14,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+
             name='Balance',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
