59dfd8564f82cdd46e607cb0a0930d831f648974
Durun <negiigainuki@gmail.com>
2019-01-27 01:50:35 +0900

    category.setName修正

--- Diamond/financial/models/category.py
+++ Diamond/financial/models/category.py
@@ -5,6 +5,7 @@ from django.db.models import Model, CharField, BooleanField
 from django.db.models import ForeignKey
 from django.db.models import CASCADE
 from .user import User
+from typing import Optional
 
 
 class Category(Model):
@@ -48,9 +49,10 @@ class Category(Model):
     def __str__(self):
         return self.name
 
-    def setName(self, name: str):
+    def setName(self, name: str) -> Optional[Model]:
         """
-        カテゴリ名をセットします
+        カテゴリ名をセットし、カテゴリ自身を返します。
+        nameが空欄のときは、セットせず、Noneを返します。
 
         Parameters
         ----------
@@ -59,8 +61,13 @@ class Category(Model):
 
         Returns
         -------
-        category : Category
-            このカテゴリオブジェクト
+        category : Category or None
+            セット成功時、このカテゴリオブジェクト
+            セット失敗時、None
         """
-        self.name = name
-        return self
+        category = self if name else None
+
+        if category:
+            category.name = name
+
+        return category
