4c466e9cf1cc5ee9897a04f4c1636fce8ccd17e6
Durun <negiigainuki@gmail.com>
2019-01-27 01:53:32 +0900

    カテゴリ削除処理を追加

--- Diamond/financial/view/categoryEdit.py
+++ Diamond/financial/view/categoryEdit.py
@@ -13,25 +13,63 @@ def main(request) -> HttpResponse:
     カテゴリ編集画面
     categoryedit/
     """
+    # プルダウンで選択されたカテゴリオブジェクトを取得
+    select = __getSelect(request)
+    error = () if select else ("categorySubscribeError", "category")
+
+    # 押したボタンによって操作を場合分け
+    keys = request.POST.keys()
+    if not select:  # カテゴリが正しく選択されていないとき
+        result = __topView(request, *error)
+    elif "delete" in keys:
+        result = __delete(request, category=select)
+    elif "change" in keys:
+        result = __change(request, category=select)
+
+    return result
+
+
+def __getSelect(request) -> Optional[Category]:
+    """
+    プルダウンで選択されたカテゴリを、DBから取得する。
+    自分の所有するカテゴリでない場合は取得しない。
+
+    Returns
+    -------
+    select : Category or None
+        選択されたカテゴリ
+    """
     username = request.session["name"]
     user = User.objects.filter(name=username).first()
-    categories = Category.objects.filter(writer=user)
-    post = request.POST
+    id = request.POST.get("categoryID")
+    select = Category.objects.filter(writer=user, id=id).first()
+    return select
 
-    id = post.get("categoryID")
-    name = post.get("categoryName")
 
-    category = categories.filter(id=id).first()  # nullable
+def __change(request, category: Category) -> HttpResponse:
+    """
+    カテゴリ名を変更し、DBへ保存する処理。
 
-    error = ()
-    if not category:
-        # 対象のカテゴリが見つからない, 自分のものでないとき
-        error = ("categorySubscribeError", "category")
-    elif not name:
-        # カテゴリ名が空欄のとき
-        error = ("categorySubscribeError", "blank")
-    else:
-        category.setName(name)
-        category.save()
-
+    Returns
+    -------
+    result
+        トップページ
+    """
+    name = request.POST.get("categoryName")
+    changedCategory = category.setName(name)
+    changedCategory and changedCategory.save()
+    error = () if changedCategory else ("categorySubscribeError", "blank")
     return __topView(request, *error)
+
+
+def __delete(request, category: Category) -> HttpResponse:
+    """
+    カテゴリを削除する処理。
+
+    Returns
+    -------
+    result
+        トップページ
+    """
+    category.delete()
+    return __topView(request)
